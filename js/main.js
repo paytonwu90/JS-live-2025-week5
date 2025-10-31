de623a396fc661d76c1985ad908b36b1976d8fae
paytonwu90 <sw9008k@gmail.com>
2025-10-31 18:10:46 +0800

    feat: 新增"填入套票內容"按鈕，自動從 testData.js 中抓資料並填入表單
    這樣比較方便測試

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { getData, addNewTicket, filterTickets } from './data.js';
 import { renderTicketCardArea, getFormData, resetForm } from './dom.js';
+import { testData } from './testData.js';
 
 
 function initEventListeners() {
@@ -33,6 +34,22 @@ function initEventListeners() {
     regionSelect.value = '全部地區';
 
     alert('新增套票成功');
+  }
+
+
+  const autoFillTicketBtn = document.querySelector('.autoFillTicket-btn');
+  autoFillTicketBtn.addEventListener('click', handleAutoFillForm);
+
+  // 測試用，自動從 testData 裡的陣列隨機取出一筆物件，並將資料填入表單
+  function handleAutoFillForm() {
+    const randomTicket = testData[Math.floor(Math.random() * testData.length)];
+    document.querySelector('#ticketName').value = randomTicket.name;
+    document.querySelector('#ticketImgUrl').value = randomTicket.imgUrl;
+    document.querySelector('#ticketRegion').value = randomTicket.area;
+    document.querySelector('#ticketPrice').value = randomTicket.price;
+    document.querySelector('#ticketNum').value = randomTicket.group;
+    document.querySelector('#ticketRate').value = randomTicket.rate;
+    document.querySelector('#ticketDescription').value = randomTicket.description;
   }
 }
 
---
+++ js/testData.js
@@ -0,0 +1,115 @@
+// 方便測試用
+const testData = [
+  {
+    "id": 3,
+    "name": "高雄愛河夜遊浪漫2日",
+    "imgUrl": "https://images.unsplash.com/photo-1557973557-ddfa9ee8c5bf?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=774",
+    "area": "高雄",
+    "description": "夜遊愛河，欣賞兩岸燈光秀，體驗高雄港都的浪漫風情。",
+    "group": 45,
+    "price": 1899,
+    "rate": 8
+  },
+  {
+    "id": 4,
+    "name": "台北101觀景台門票",
+    "imgUrl": "https://images.unsplash.com/photo-1609147110688-83b5fd1288e8?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=870",
+    "area": "台北",
+    "description": "登上台北101觀景台，360度俯瞰台北市全景，感受城市天際線的壯觀。",
+    "group": 120,
+    "price": 600,
+    "rate": 9
+  },
+  {
+    "id": 5,
+    "name": "台中彩虹眷村半日遊",
+    "imgUrl": "https://images.unsplash.com/photo-1705312390462-bca179021d0c?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8JUU1JUJEJUE5JUU4JTk5JUI5JUU3JTlDJUI3JUU2JTlEJTkxfGVufDB8fDB8fHww&auto=format&fit=crop&q=60&w=600",
+    "area": "台中",
+    "description": "走訪充滿童趣的彩虹眷村，感受繽紛彩繪與懷舊建築的完美結合。",
+    "group": 35,
+    "price": 350,
+    "rate": 8
+  },
+  {
+    "id": 6,
+    "name": "高雄西子灣夕陽遊",
+    "imgUrl": "https://bobbyfun.tw/wp-content/uploads/pixnet/b39465f48bf4952e315c1d0500da8b5f.jpg",
+    "area": "高雄",
+    "description": "在西子灣欣賞全台最美夕陽，品嚐在地海鮮美食。",
+    "group": 28,
+    "price": 1200,
+    "rate": 7
+  },
+  {
+    "id": 7,
+    "name": "台北故宮博物院門票",
+    "imgUrl": "https://ak-d.tripcdn.com/images/fd/tg/g4/M01/72/7D/CggYHFY7F7iAF6-bAAtq3wFHDJY069.jpg",
+    "area": "台北",
+    "description": "探索中華文化瑰寶，欣賞故宮博物院豐富的文物收藏。",
+    "group": 75,
+    "price": 350,
+    "rate": 9
+  },
+  {
+    "id": 8,
+    "name": "高雄駁二藝術特區一日遊",
+    "imgUrl": "https://khh.travel/image/55818/",
+    "area": "高雄",
+    "description": "探索高雄最具代表性的文創園區，欣賞街頭藝術與文創展覽。",
+    "group": 42,
+    "price": 299,
+    "rate": 8
+  },
+  {
+    "id": 9,
+    "name": "台中高美濕地夕陽遊",
+    "imgUrl": "https://travel.taichung.gov.tw/Image/12578?r=637837893158126667",
+    "area": "台中",
+    "description": "在風車與夕陽的見證下，漫步高美濕地木棧道，欣賞絕美日落。",
+    "group": 35,
+    "price": 450,
+    "rate": 9
+  },
+  {
+    "id": 10,
+    "name": "台北象山親山步道半日遊",
+    "imgUrl": "https://www.travel.taipei/image/222132/?r=1637654386987",
+    "area": "台北",
+    "description": "登頂象山，俯瞰台北101與城市天際線，捕捉最美夜景。",
+    "group": 25,
+    "price": 199,
+    "rate": 8
+  },
+  {
+    "id": 11,
+    "name": "高雄旗津單車環島遊",
+    "imgUrl": "https://khh.travel/image/20066/",
+    "area": "高雄",
+    "description": "騎乘單車暢遊旗津半島，品嚐道地海鮮美食，感受港都風情。",
+    "group": 30,
+    "price": 350,
+    "rate": 8
+  },
+  {
+    "id": 12,
+    "name": "台中宮原眼科冰淇淋體驗",
+    "imgUrl": "https://www.miyahara.com.tw/images/shopinfo/piclist/pic1414.jpg",
+    "area": "台中",
+    "description": "在古蹟建築中品嚐創意冰淇淋，享受視覺與味覺的雙重饗宴。",
+    "group": 20,
+    "price": 200,
+    "rate": 7
+  },
+  {
+    "id": 13,
+    "name": "台北北投溫泉體驗",
+    "imgUrl": "https://www.travel.taipei/image/176116/?r=1588659846943",
+    "area": "台北",
+    "description": "放鬆身心，享受北投溫泉的療癒時光，體驗日式泡湯文化。",
+    "group": 15,
+    "price": 800,
+    "rate": 9
+  }
+];
+
+export { testData };
